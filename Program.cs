aaceca21d667d0be67f9bd06f6b47bf92e3a0960
Trey Suiter <trey.suiter@gmail.com>
2022-04-28 18:40:09 -0500

    Multi and jagged array fun

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace DataStructuresAndAlgorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Multidimensional array's length will multiple the dimensions
+            int[,] multiArray = new int[2, 3];
+            int multiArrValue = multiArray[0, 2];
+
+            Console.WriteLine(multiArray.Length);
+
+            //Jagged arra's length is just the number of columns
+            int[][] jagArray = new int[2][];
+            jagArray[0] = new int[3];
+
+            Console.WriteLine(jagArray.Length);
+
+
         }
     }
 }
